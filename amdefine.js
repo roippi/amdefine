cce59815c27bdcbbb3bf0e69878daeebee1d5e22
Ben Roberts <beroberts@wayfair.com>
2017-01-01 10:35:30 -0500

    treat 3-arg define() calls like 1- and 2- arg

--- amdefine.js
+++ amdefine.js
@@ -147,28 +147,18 @@ function amdefine(module, requireFn) {
     function runFactory(id, deps, factory) {
         var r, e, m, result;
 
-        if (id) {
-            e = loaderCache[id] = {};
-            m = {
-                id: id,
-                uri: __filename,
-                exports: e
-            };
-            r = makeRequire(requireFn, e, m, id);
-        } else {
-            //Only support one define call per file
-            if (alreadyCalled) {
-                throw new Error('amdefine with no module ID cannot be called more than once per file.');
-            }
-            alreadyCalled = true;
-
-            //Use the real variables from node
-            //Use module.exports for exports, since
-            //the exports in here is amdefine exports.
-            e = module.exports;
-            m = module;
-            r = makeRequire(requireFn, e, m, module.id);
+        //Only support one define call per file
+        if (alreadyCalled) {
+            throw new Error('amdefine with no module ID cannot be called more than once per file.');
         }
+        alreadyCalled = true;
+
+        //Use the real variables from node
+        //Use module.exports for exports, since
+        //the exports in here is amdefine exports.
+        e = module.exports;
+        m = module;
+        r = makeRequire(requireFn, e, m, module.id);
 
         //If there are dependencies, they are strings, so need
         //to convert them to dependency values.
@@ -270,12 +260,9 @@ function amdefine(module, requireFn) {
         //internal cache. If no ID, then use the external variables
         //for this node module.
         if (id) {
-            //Put the module in deep freeze until there is a
-            //require call for it.
             defineCache[id] = [id, deps, factory];
-        } else {
-            runFactory(id, deps, factory);
-        }
+        } 
+        runFactory(id, deps, factory);
     }
 
     //define.require, which has access to all the values in the
